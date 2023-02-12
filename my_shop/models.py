f8da288bb8c63c441d9cd9821f40690ef99f9051
BliahaMuha <hramychev123@gmail.com>
2023-02-12 23:44:50 +0300

    Добавлены шаблоны HTML

--- my_shop/models.py
+++ my_shop/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 
 class Category(models.Model):
@@ -17,6 +17,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('my_shop:product_list_by_category', args=[self.slug])
 
 
 class Product(models.Model):
@@ -44,3 +47,5 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse('my_shop:product_detail', args=[self.id, self.slug])
