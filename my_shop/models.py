93fa018e595cb067fdaeb6d1b5a3bbfe8d589fbb
BliahaMuha <116337765+BliahaMuha@users.noreply.github.com>
2023-02-13 13:51:32 +0300

    Add files via upload

---
+++ my_shop/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.urls import reverse
+# Create your models here.
+
+class Category(models.Model):
+    """
+    Класс-описание
+    модели Категорий товаров
+    """
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ('name', )
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('my_shop:product_list_by_category', args=[self.slug])
+
+
+class Product(models.Model):
+    """
+    Класс-описание товаров
+    """
+    category = models.ForeignKey(Category,
+                                 related_name='products',
+                                 on_delete=models.CASCADE)
+    name = models.CharField(max_length=150, db_index=True)
+    slug = models.CharField(max_length=150, db_index=True, unique=True)
+    image = models.ImageField(upload_to='product/%Y/%m/%d', blank=True)
+    description = models.TextField(max_length=1000, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    uploaded = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name', )
+        verbose_name = 'Товар',
+        verbose_name_plural = 'Товары'
+        index_together = (('id', 'slug'), )
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('my_shop:product_detail', args=[self.id, self.slug])
